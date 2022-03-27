c87b65c64ec645f4685548388ef3632625dfc2b3
Ale Bullo <bullorinia@gmail.com>
2022-03-27 02:17:17 -0300

    active currency and icon

--- src/layouts/CurrencySelector/CurrencySelector.js
+++ src/layouts/CurrencySelector/CurrencySelector.js
@@ -1,23 +1,43 @@
 import React from "react";
 import CurrencyContext from "../../services/context/currencyContext";
+import "./CurrencySelector.css";
+import arrowDown from "../../assets/icons/angle-down-solid.svg";
+import styled from "styled-components";
+
+const Currency = styled.div`
+  width: 40px;
+  margin-right: 10px;
+  
+  font-weight: 500;
+  font-size: 18px;
+  
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
 
 class CurrencySelector extends React.Component {
-    static contextType = CurrencyContext;
+  static contextType = CurrencyContext;
 
-    componentDidMount(){
-      console.log('CURRENCY SELECTOR ',this.context);
-    }
-// grab context Currency, show selectedCurrency
-// render PopUp
-
+  componentDidMount() {
+    console.log("CURRENCY SELECTOR ", this.context);
+  }
+  // grab context Currency, show selectedCurrency
+  // render PopUp
 
   render() {
-    
     return (
-      <div className="CurrencySelector">
-          {this.context.selectedCurrency.symbol}
-          {this.context.selectedCurrency.label}<br/>
-      </div>
+      this.context.selectedCurrency && (
+        <section className="CurrencySelector">
+          
+          <Currency className="selected-currency">
+            
+            {this.context.selectedCurrency.symbol}
+            <img src={arrowDown} alt="Arrow icon" className="arrow-icn" />
+
+          </Currency>
+        </section>
+      )
     );
   }
 }
--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -4,28 +4,24 @@ import { Link } from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
 import CurrencySelector from "../CurrencySelector/CurrencySelector";
 import CartSnippet from "../CartSnippet/CartSnippet";
-import logo from '../../assets/images/a-logo.png'
-import './Header.css'
+import logo from "../../assets/images/a-logo.png";
+import "./Header.css";
 
 class Header extends React.Component {
-    
-    constructor(){
-        super()
-        this.state = {menuCategories:null}
-    }
-  
-    async componentDidMount(){
-    client.setEndpoint('http://localhost:4000')
-           
-    const getCategories = new Query('categories', true)
-        .addFieldList(['name'])
-    
-    const {categories} = await client.post(getCategories)
+  constructor() {
+    super();
+    this.state = { menuCategories: null };
+  }
 
-    this.setState({menuCategories:categories}) 
+  async componentDidMount() {
+    client.setEndpoint("http://localhost:4000");
 
-    }
+    const getCategories = new Query("categories", true).addFieldList(["name"]);
 
+    const { categories } = await client.post(getCategories);
+
+    this.setState({ menuCategories: categories });
+  }
 
   // render CurrencySelector
   // render CartSnippet
@@ -33,15 +29,19 @@ class Header extends React.Component {
   render() {
     return (
       <header className="Header">
+        {this.state.menuCategories && (
+          <NavBar categories={this.state.menuCategories} />
+        )}
 
-        {this.state.menuCategories && <NavBar categories={this.state.menuCategories} />}
-        
-        <Link to='/plp/all'><img src={logo} alt='Logo for coding challange for Scandiweb'/></Link>
-        
-        <CurrencySelector />
-        
-        <CartSnippet />
-        
+        <Link to="/plp/all">
+          <img src={logo} alt="Logo for coding challange for Scandiweb" />
+        </Link>
+
+        <div className="picker-container">
+          <CurrencySelector />
+
+          <CartSnippet />
+        </div>
       </header>
     );
   }
