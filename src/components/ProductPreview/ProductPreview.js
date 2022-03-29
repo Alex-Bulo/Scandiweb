cdb3bcb79daf91c77cc53ff17fada939409f5cf2
Ale Bullo <bullorinia@gmail.com>
2022-03-29 01:37:01 -0300

    styling transitions to enabled product preview

--- src/components/ProductPreview/ProductPreview.js
+++ src/components/ProductPreview/ProductPreview.js
@@ -3,38 +3,34 @@ import CurrencyContext from "../../services/context/currencyContext";
 import Carrousel from "../Carrousel/Carrousel";
 import Price from "../Price/Price";
 import "./ProductPreview.css";
-
+import cartIcn from "../../assets/icons/white-cart.svg";
 class ProductPreview extends React.Component {
   static contextType = CurrencyContext;
   constructor(props) {
     super(props);
     this.state = { active: null, qty: null, selectedAttribute: null };
-    this.enableProductHandler = this.enableProductHandler.bind(this)
+    this.enableProductHandler = this.enableProductHandler.bind(this);
   }
 
-  componentDidMount(){
-    this.setState({active:false, qty:1, selectedAttribute:0})
-
+  componentDidMount() {
+    this.setState({ active: false, qty: 1, selectedAttribute: 0 });
   }
 
-  enableProductHandler (){
-    this.props.product.inStock && this.setState({...this.state, active:true})
+  enableProductHandler() {
+    this.props.product.inStock &&
+      this.setState({ ...this.state, active: !this.state.active });
   }
-
-
 
   render() {
-    
     const { inStock, name, brand, prices, gallery } = this.props.product;
-    const {active, qty, selectedAttribute} = this.state
+    const { active, qty, selectedAttribute } = this.state;
 
     return (
       this.context.selectedCurrency && (
         <article
-          className={`ProductPreview ${!inStock ? "without-stock" : ""} ${active ? 'enabled-product':''}`}
-          onClick={()=>this.enableProductHandler()}
-          onMouseEnter={()=>this.enableProductHandler()}
-          onMouseLeave={()=>this.setState({...this.state,active:false})}
+          className={`ProductPreview ${!inStock ? "without-stock" : ""} ${active ? "enabled-product" : ""}`}
+          onClick={() => this.enableProductHandler()}
+          onMouseLeave={() => this.setState({ ...this.state, active: false })}
         >
           <Carrousel
             inStock={inStock}
@@ -44,15 +40,50 @@ class ProductPreview extends React.Component {
           />
 
           <section className="preview-content">
-            <h2 className="preview-title">
-              {brand} - {name}
-            </h2>
+            <h2 className="preview-title"> {brand} - {name} </h2>
             <Price
               prices={prices}
               qty={qty}
               currency={this.context.selectedCurrency.label}
             />
           </section>
+
+          {active && (
+            <section>
+              <div
+                className=" cta-primary add-cart-container"
+                onClick={() => alert("hola")}
+              >
+                <img
+                  src={cartIcn}
+                  alt="Add to cart button"
+                  className="add-cart-icon"
+                />
+              </div>
+              <div
+                style={{
+                  width: "100%",
+                  height: "55px",
+                  backgroundColor: "#000",
+                  boxShadow: "0px 4px 35px rgba(168, 172, 176, 0.19)",
+                  zIndex: "8",
+                }}
+              >
+                att
+              </div>
+              <div
+                style={{
+                  width: "100%",
+                  height: "55px",
+                  backgroundColor: "red",
+                  boxShadow: "0px 4px 35px rgba(168, 172, 176, 0.19)",
+                  zIndex: "100",
+                }}
+              >
+                ocounter
+              </div>
+            </section>
+          )}
         </article>
       )
     );
